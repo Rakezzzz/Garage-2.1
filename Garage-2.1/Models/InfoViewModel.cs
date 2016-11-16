909740bd24b668fb335399439dc977e9c94c913f
antewest <ante83@hotmail.com>
2016-11-16 15:25:37 +0100

    RentSpot

--- Garage-2.1/Models/InfoViewModel.cs
+++ Garage-2.1/Models/InfoViewModel.cs
@@ -8,14 +8,20 @@ namespace Garage_2._1.Models
 {
     public class InfoViewModel
     {
-        public int ParkingSpotId { get; private set; }
-        public Person Owner { get; private set; }
-        public IEnumerable Collection { get; private set; }
+        public int ParkingSpotId { get; set; }
+        public Person Owner { get; set; }
+        public IEnumerable Collection { get; set; }
+        public TimeSpan Time {get; set;}
 
-        public InfoViewModel(int parkingspotId, Person owner = null, IEnumerable collection = null)
+        public InfoViewModel()
+        {
+            
+        }
+
+        public InfoViewModel(int parkingspotId, IEnumerable collection = null, TimeSpan time = default(TimeSpan))
         {
             this.ParkingSpotId = parkingspotId;
-            this.Owner = (Person)owner.Clone();
+            this.Owner = new Person();
             this.Collection = collection;
         }
     }

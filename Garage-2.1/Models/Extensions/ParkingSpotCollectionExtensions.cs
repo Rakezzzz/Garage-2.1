7519565144ba90f88728cb3630c435428025c835
Emil Eklund <emileklund@outlook.com>
2016-11-18 10:54:29 +0100

    Moving Filter to Extension

---
+++ Garage-2.1/Models/Extensions/ParkingSpotCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Garage_2._1.Models;
+
+namespace Garage_2._1.Models.Extensions
+{
+    public static class ParkingSpotCollectionExtensions
+    {
+        public IEnumerable<Parkingspot> Filter(this IEnumerable<Parkingspot> parkingSpots, VehicleType? vehicleType = null, TimeSpan? timeSpan = null)
+        {
+            IEnumerable<Parkingspot> filteredCars = null;
+
+            if (vehicleType == null && timeSpan.HasValue)
+                filteredCars = from car in parkingSpots
+                               where car.TimeOfRental < DateTime.Now.Subtract(timeSpan.Value)
+                               select car;
+
+            else if (timeSpan == null && vehicleType.HasValue)
+            {
+                filteredCars = from car in parkingSpots
+                               where car.ParkedVehicle.Type == vehicleType
+                               select car;
+            }
+            else if (vehicleType.HasValue && timeSpan.HasValue)
+            {
+                filteredCars = from car in parkingSpots
+                               where (car.ParkedVehicle.Type == vehicleType) && (car.TimeOfRental < DateTime.Now.Subtract(timeSpan.Value))
+                               select car;
+
+            }
+            return filteredCars;
+        }
+    }
+}
--- Garage-2.1/Repositories/ParkingSpotRepository.cs
+++ Garage-2.1/Repositories/ParkingSpotRepository.cs
@@ -234,42 +234,5 @@ namespace Garage_2._1.Repositories
 
         }
 
-        public IEnumerable<Parkingspot> Filter(List<Parkingspot> parkingSpots, VehicleType? vehicleType = null, TimeSpan? timeSpan = null)
-        {
-            IEnumerable<Parkingspot> filteredCars = null;
-
-            if (vehicleType == null && timeSpan.HasValue)
-                filteredCars = from car in parkingSpots
-                               where car.TimeOfRental < DateTime.Now.Subtract(timeSpan.Value)
-                               select car;
-
-            else if (timeSpan == null && vehicleType.HasValue)
-            {
-                foreach (var item in parkingSpots)
-                {
-                    item.ParkedVehicle = GetVehicleByRegNum(item.RegNum);
-                }
-
-                filteredCars = from car in parkingSpots
-                               where car.ParkedVehicle.Type == vehicleType
-                               select car;
-            }
-            else if (vehicleType.HasValue && timeSpan.HasValue)
-            {
-                foreach (var item in parkingSpots)
-                {
-                    item.ParkedVehicle = GetVehicleByRegNum(item.RegNum);
-                }
-
-                filteredCars = from car in parkingSpots
-                               where (car.ParkedVehicle.Type == vehicleType) && (car.TimeOfRental < DateTime.Now.Subtract(timeSpan.Value))
-                               select car;
-
-            }
-
-
-            return filteredCars;
-        }
-
     }
 }
